5c81be017d6438ae412a8329bae4d3089c23e96b
Ryba218 <vijock@gmail.com>
2020-05-05 20:37:43 +0200

    Calculator

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,29 @@
+package com.kodilla;
+
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+import java.sql.SQLOutput;
+
+public class Calculator {
+
+        public double adding(double x,double y) {
+        double addingResult = x + y;
+        return addingResult;
+        }
+        public double substraction(double x,double y) {
+        double substractionResult = x - y;
+        return substractionResult;
+        }
+    public static void main(String[] args) {
+         Calculator calculator = new Calculator();
+         double addResult = calculator.adding(2.6,3.8);
+         System.out.println("Adding result: " + addResult);
+         double subResult = calculator.substraction(4.5,2.2);
+         System.out.println("Substraction result: " + subResult);
+
+
+    }
+
+}
+
+
