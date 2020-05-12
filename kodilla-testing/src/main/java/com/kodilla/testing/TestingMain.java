020cdb2a6725081da583e44e354955e2df545f09
Ryba218 <vijock@gmail.com>
2020-05-12 09:11:52 +0200

    Calculator Test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,33 @@
+
 package com.kodilla.testing;
+
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+
+        int a = 11;
+        int b = 7;
+
+        int expectedAddResult = a + b;
+        int expectedSubResult = a - b;
+
+
+        Calculator test = new Calculator();
+
+        if (expectedAddResult == test.add(a,b)) {
+            System.out.println("Adding OK");
+        }  else {
+            System.out.println("Adding isn't OK");
+        }
+
+        if (expectedSubResult == test.substract(a,b)) {
+            System.out.println("Substraction OK");
+        }  else {
+            System.out.println("Substraction failed");
+        }
+
+
+        
     }
 }
