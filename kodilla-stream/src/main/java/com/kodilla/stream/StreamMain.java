2120e135539b96157df15071dc9d12eee2a3c337
Ryba218 <vijock@gmail.com>
2020-06-16 13:25:31 +0200

    Immutable

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,6 +2,7 @@ package com.kodilla.stream;
 
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.Executor;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.FunctionalCalculator;
@@ -10,6 +11,21 @@ import com.kodilla.stream.lambda.Processor;
 
 public class StreamMain {
     public static void main(String[] args) {
+
+
+        //ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        //System.out.println("Calculating expressions with method references");
+        //expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        //expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        //expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        //expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        // expressionExecutor.executeExpression(10,5, (a,b) -> a + b);
+        // expressionExecutor.executeExpression(10,5, (a,b) -> a - b);
+        // expressionExecutor.executeExpression(10,5, (a,b) -> a * b);
+        //expressionExecutor.executeExpression(10,5,(a,b)-> a/b);
+
 
         PoemBeautifier poemBeautifier = new PoemBeautifier();
 
@@ -29,21 +45,9 @@ public class StreamMain {
         System.out.println(betterText5);
 
 
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
-
-
-        //ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-
-        //System.out.println("Calculating expressions with method references");
-        //expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        //expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        //expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        //expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
-
-       // expressionExecutor.executeExpression(10,5, (a,b) -> a + b);
-       // expressionExecutor.executeExpression(10,5, (a,b) -> a - b);
-       // expressionExecutor.executeExpression(10,5, (a,b) -> a * b);
-        //expressionExecutor.executeExpression(10,5,(a,b)-> a/b);
 
 
 
